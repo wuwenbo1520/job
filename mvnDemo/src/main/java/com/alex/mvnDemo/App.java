e7440b42c2986fb4810ec26aeeaeb1e7aea023e7
Your Name <you@example.com>
2020-01-04 21:23:37 +0800

    123

---
+++ mvnDemo/src/main/java/com/alex/mvnDemo/App.java
@@ -0,0 +1,18 @@
+package com.alex.mvnDemo;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+        System.out.println( "Hello World 2 " );
+        System.out.println( "Hello World 3 " );
+        System.out.println( "Hello World 4 " );
+        System.out.println( "Hello docker 5 " );
+        System.out.println( "Hello docker 2 " );
+    }
+}
